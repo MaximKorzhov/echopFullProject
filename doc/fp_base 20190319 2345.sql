904b754f09119caeb2981191e45262a9f377ef28
dmkorolev@yandex.ru <dmkorolev@yandex.ru>
2019-03-19 23:48:12 +0300

    restructure base

--- doc/fp_base 20190319 2345.sql
+++ doc/fp_base 20190319 2345.sql
@@ -1,7 +1,7 @@
 ﻿--
 -- Скрипт сгенерирован Devart dbForge Studio 2019 for MySQL, Версия 8.1.22.0
 -- Домашняя страница продукта: http://www.devart.com/ru/dbforge/mysql/studio
--- Дата скрипта: 18.03.2019 22:26:29
+-- Дата скрипта: 19.03.2019 23:45:18
 -- Версия сервера: 8.0.15
 -- Версия клиента: 4.1
 --
@@ -32,11 +32,6 @@ USE fp_base;
 DROP TABLE IF EXISTS country;
 
 --
--- Удалить таблицу `groups`
---
-DROP TABLE IF EXISTS groups;
-
---
 -- Удалить таблицу `messages`
 --
 DROP TABLE IF EXISTS messages;
@@ -47,24 +42,24 @@ DROP TABLE IF EXISTS messages;
 DROP TABLE IF EXISTS migration;
 
 --
+-- Удалить таблицу `order`
+--
+DROP TABLE IF EXISTS `order`;
+
+--
 -- Удалить таблицу `position`
 --
 DROP TABLE IF EXISTS `position`;
 
 --
--- Удалить таблицу `shops`
+-- Удалить таблицу `organizations`
 --
-DROP TABLE IF EXISTS shops;
+DROP TABLE IF EXISTS organizations;
 
 --
--- Удалить таблицу `suppliers`
+-- Удалить таблицу `org_type`
 --
-DROP TABLE IF EXISTS suppliers;
-
---
--- Удалить таблицу `zakaz`
---
-DROP TABLE IF EXISTS zakaz;
+DROP TABLE IF EXISTS org_type;
 
 --
 -- Удалить таблицу `user`
@@ -72,32 +67,9 @@ DROP TABLE IF EXISTS zakaz;
 DROP TABLE IF EXISTS user;
 
 --
--- Удалить таблицу `user_type`
---
-DROP TABLE IF EXISTS user_type;
-
---
 -- Установка базы данных по умолчанию
 --
 USE fp_base;
-
---
--- Создать таблицу `user_type`
---
-CREATE TABLE user_type (
-  id int(11) DEFAULT NULL,
-  name varchar(50) DEFAULT NULL
-)
-ENGINE = INNODB,
-AVG_ROW_LENGTH = 8192,
-CHARACTER SET utf8,
-COLLATE utf8_general_ci;
-
---
--- Создать индекс `UK_user`.`type_id` для объекта типа таблица `user_type`
---
-ALTER TABLE user_type
-ADD UNIQUE INDEX `UK_user.type_id` (id);
 
 --
 -- Создать таблицу `user`
@@ -112,7 +84,7 @@ CREATE TABLE user (
   status smallint(6) NOT NULL DEFAULT 10,
   created_at int(11) NOT NULL,
   updated_at int(11) NOT NULL,
-  user_type_id int(11) DEFAULT NULL,
+  user_type_id int(11) NOT NULL DEFAULT 0,
   tel varchar(255) DEFAULT NULL,
   name varchar(50) DEFAULT NULL,
   last varchar(255) DEFAULT NULL,
@@ -131,6 +103,12 @@ ALTER TABLE user
 ADD UNIQUE INDEX email (email);
 
 --
+-- Создать индекс `FK_user_user_type_id` для объекта типа таблица `user`
+--
+ALTER TABLE user
+ADD INDEX FK_user_user_type_id (user_type_id);
+
+--
 -- Создать индекс `password_reset_token` для объекта типа таблица `user`
 --
 ALTER TABLE user
@@ -143,46 +121,36 @@ ALTER TABLE user
 ADD UNIQUE INDEX username (username);
 
 --
--- Создать внешний ключ
+-- Создать таблицу `org_type`
 --
-ALTER TABLE user
-ADD CONSTRAINT FK_user_user_type_id FOREIGN KEY (user_type_id)
-REFERENCES user_type (id);
-
---
--- Создать таблицу `zakaz`
---
-CREATE TABLE zakaz (
-  zakaz_id int(11) NOT NULL AUTO_INCREMENT,
-  zakaz_from int(11) NOT NULL,
-  zakaz_to int(11) NOT NULL,
-  position_id int(11) NOT NULL,
-  date_from datetime DEFAULT NULL,
-  date_to datetime DEFAULT NULL,
-  state varchar(45) DEFAULT NULL,
-  soob_id varchar(45) DEFAULT NULL,
-  zak_id int(11) NOT NULL,
-  summ varchar(45) DEFAULT NULL,
-  PRIMARY KEY (zakaz_id),
-  UNIQUE INDEX idnew_table_UNIQUE (zakaz_id)
+CREATE TABLE org_type (
+  id int(11) DEFAULT NULL,
+  name varchar(50) DEFAULT NULL
 )
 ENGINE = INNODB,
-AUTO_INCREMENT = 2,
+AVG_ROW_LENGTH = 8192,
 CHARACTER SET utf8,
 COLLATE utf8_general_ci;
 
 --
--- Создать таблицу `suppliers`
+-- Создать индекс `UK_user`.`type_id` для объекта типа таблица `org_type`
 --
-CREATE TABLE suppliers (
+ALTER TABLE org_type
+ADD UNIQUE INDEX `UK_user.type_id` (id);
+
+--
+-- Создать таблицу `organizations`
+--
+CREATE TABLE organizations (
   id int(11) NOT NULL AUTO_INCREMENT,
   unp varchar(45) DEFAULT NULL,
   bank varchar(100) DEFAULT NULL,
-  contact_id varchar(45) DEFAULT NULL,
+  user_id int(11) DEFAULT NULL,
   name varchar(45) DEFAULT NULL,
   schet varchar(45) DEFAULT NULL,
   balans varchar(45) DEFAULT NULL,
   status varchar(45) DEFAULT NULL,
+  org_type_id int(11) DEFAULT NULL,
   PRIMARY KEY (id),
   UNIQUE INDEX id_UNIQUE (id)
 )
@@ -193,37 +161,24 @@ CHARACTER SET utf8,
 COLLATE utf8_general_ci;
 
 --
--- Создать индекс `unp_UNIQUE` для объекта типа таблица `suppliers`
+-- Создать индекс `unp_UNIQUE` для объекта типа таблица `organizations`
 --
-ALTER TABLE suppliers
+ALTER TABLE organizations
 ADD UNIQUE INDEX unp_UNIQUE (unp);
 
 --
--- Создать таблицу `shops`
+-- Создать внешний ключ
 --
-CREATE TABLE shops (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  unp varchar(45) DEFAULT NULL,
-  bank varchar(45) DEFAULT NULL,
-  contact_id varchar(45) DEFAULT NULL,
-  name varchar(45) DEFAULT NULL,
-  schet varchar(45) DEFAULT NULL,
-  balans varchar(45) DEFAULT NULL,
-  status varchar(45) DEFAULT NULL,
-  PRIMARY KEY (id),
-  UNIQUE INDEX id_UNIQUE (id)
-)
-ENGINE = INNODB,
-AUTO_INCREMENT = 2,
-AVG_ROW_LENGTH = 16384,
-CHARACTER SET utf8,
-COLLATE utf8_general_ci;
+ALTER TABLE organizations
+ADD CONSTRAINT FK_organizations_org_type_id FOREIGN KEY (org_type_id)
+REFERENCES org_type (id) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Создать индекс `unp_UNIQUE` для объекта типа таблица `shops`
+-- Создать внешний ключ
 --
-ALTER TABLE shops
-ADD UNIQUE INDEX unp_UNIQUE (unp);
+ALTER TABLE organizations
+ADD CONSTRAINT FK_organizations_user_id FOREIGN KEY (user_id)
+REFERENCES user (id);
 
 --
 -- Создать таблицу `position`
@@ -240,7 +195,7 @@ CREATE TABLE `position` (
   size varchar(45) DEFAULT NULL,
   podrobno blob DEFAULT NULL,
   add_pole varchar(45) DEFAULT NULL,
-  from_id varchar(45) DEFAULT NULL,
+  org_id int(11) DEFAULT NULL,
   PRIMARY KEY (id),
   UNIQUE INDEX id_UNIQUE (id)
 )
@@ -249,6 +204,41 @@ AUTO_INCREMENT = 23,
 AVG_ROW_LENGTH = 1820,
 CHARACTER SET utf8,
 COLLATE utf8_general_ci;
+
+--
+-- Создать внешний ключ
+--
+ALTER TABLE `position`
+ADD CONSTRAINT FK_position_organizations_id FOREIGN KEY (org_id)
+REFERENCES organizations (id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Создать таблицу `order`
+--
+CREATE TABLE `order` (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  zakaz_from int(11) NOT NULL,
+  zakaz_to int(11) NOT NULL,
+  position_id int(11) NOT NULL,
+  date_from datetime DEFAULT NULL,
+  date_to datetime DEFAULT NULL,
+  state varchar(45) DEFAULT NULL,
+  soob_id varchar(45) DEFAULT NULL,
+  summ varchar(45) DEFAULT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX idnew_table_UNIQUE (id)
+)
+ENGINE = INNODB,
+AUTO_INCREMENT = 2,
+CHARACTER SET utf8,
+COLLATE utf8_general_ci;
+
+--
+-- Создать внешний ключ
+--
+ALTER TABLE `order`
+ADD CONSTRAINT FK_order_position_id FOREIGN KEY (position_id)
+REFERENCES `position` (id) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Создать таблицу `migration`
@@ -281,19 +271,6 @@ CHARACTER SET utf8,
 COLLATE utf8_general_ci;
 
 --
--- Создать таблицу `groups`
---
-CREATE TABLE groups (
-  gr_id int(11) NOT NULL AUTO_INCREMENT,
-  gr_name varchar(90) DEFAULT NULL,
-  gr_pod int(11) DEFAULT NULL,
-  PRIMARY KEY (gr_id)
-)
-ENGINE = INNODB,
-CHARACTER SET utf8,
-COLLATE utf8_general_ci;
-
---
 -- Создать таблицу `country`
 --
 CREATE TABLE country (
@@ -302,50 +279,40 @@ CREATE TABLE country (
   PRIMARY KEY (id)
 )
 ENGINE = INNODB,
+AUTO_INCREMENT = 4,
+AVG_ROW_LENGTH = 5461,
 CHARACTER SET utf8,
 COLLATE utf8_general_ci;
-
--- 
--- Вывод данных для таблицы user_type
---
-INSERT INTO user_type VALUES
-(1, 'suppliers'),
-(0, 'shops');
-
--- 
--- Вывод данных для таблицы zakaz
---
-INSERT INTO zakaz VALUES
-(1, 2, 9, 2, '2021-02-20 19:00:00', '2024-02-20 19:00:00', 'open', NULL, 0, '100');
 
 -- 
 -- Вывод данных для таблицы user
 --
 INSERT INTO user VALUES
-(3, 'max', 'HKFUNmQW9LNkzmpWNpU2h4sLoaVFoROe', '$2y$13$ScC2G.hJ1xekn//QFRd8sOvM/2SUx5.pAA5PilY.v0eYz8/KxTZ5G', NULL, 'ironmax68@yandex.ru', 10, 1552162089, 1552162089, 0, NULL, NULL, NULL),
-(4, 'maks', '1234567', '$2y$13$ck8XSy/lRhgSX7fvWQFfieuMmR2daGnrT.W5ZitIu4roKDDXY3s6u', '55555555555555', 'ironmax63@yandex.ru', 2, 2, 2, 0, NULL, NULL, NULL),
-(5, 'dima', '123', '1211', '2323', 'fsds@sdfswfs', 1, 1, 2, 1, NULL, NULL, NULL);
+(1, 'admin', 'admin', '$2y$13$fYXItsLXsudX1PjSP4Ez4OXyp3THYHv9tkA2kNC7V7aZsrFdr8b3q', NULL, 'ya.ru', 10, 1, 1, 0, NULL, NULL, NULL),
+(6, 'user', 'user', 'asdfwsd', NULL, 'y.ru', 10, 1, 1, 1, NULL, NULL, NULL),
+(7, 'user2', 'user2', 'sdvdw', NULL, 'tt.ru', 10, 1, 1, 0, NULL, NULL, NULL);
 
 -- 
--- Вывод данных для таблицы suppliers
+-- Вывод данных для таблицы org_type
 --
-INSERT INTO suppliers VALUES
-(9, '54352', NULL, NULL, 'Zаумыу', NULL, NULL, NULL),
-(10, '741236', NULL, NULL, 'ООО &quot;Фабрика&quot;', NULL, NULL, NULL),
-(11, 'fvds', NULL, NULL, 'ООО &quot;Фабрика&quot;', NULL, NULL, NULL),
-(12, 'п54ц4', NULL, NULL, 'п5кы', NULL, NULL, NULL);
+INSERT INTO org_type VALUES
+(1, 'suppliers'),
+(0, 'shops');
 
 -- 
--- Вывод данных для таблицы shops
+-- Вывод данных для таблицы organizations
 --
-INSERT INTO shops VALUES
-(1, '123', '456', '765', 'один', '13232323', '233222', 'ыфф');
+INSERT INTO organizations VALUES
+(2, '741236', NULL, 7, 'ООО Фабрика', NULL, NULL, NULL, NULL),
+(9, '54352', NULL, 6, 'Zаумыу', NULL, NULL, NULL, NULL),
+(11, 'fvds', NULL, 1, 'ООО Завод', NULL, NULL, NULL, NULL),
+(12, 'п54ц4', NULL, NULL, 'п5кы', NULL, NULL, NULL, NULL);
 
 -- 
 -- Вывод данных для таблицы `position`
 --
 INSERT INTO `position` VALUES
-(2, '5898', '286868486687', 'товар 3', 1.24, '2019-02-04 22:55:33', 'rdtrп', 'yufyj', '444x776x888', x'6B6A6664736864206B682064696775656877726C696768777269756C67686577726C67726567772C726520686A756C726B6575206768777265752069676975726520676869756577726C206768696C7565727720676869756577722067756972657772207767206466676264666720617265672065726720657220676572206720', '9', NULL),
+(2, '5898', '286868486687', 'товар 3', 1.24, '2019-02-04 22:55:33', 'rdtrп', 'yufyj', '444x776x888', x'6B6A6664736864206B682064696775656877726C696768777269756C67686577726C67726567772C726520686A756C726B6575206768777265752069676975726520676869756577726C206768696C7565727720676869756577722067756972657772207767206466676264666720617265672065726720657220676572206720', '9', 9),
 (6, 'cxvx', '45354', 'gdgd', 3.10, '2019-02-19 13:38:53', 'efwe', NULL, 'gerge', x'3234', '9', NULL),
 (7, 'cxvx', '45354', 'gdgd', 3.00, '2019-02-18 12:39:31', 'efwe', NULL, 'gerge', x'', '9', NULL),
 (13, '65465', '345435345', 'jhjh', 2.13, '2019-02-19 20:56:01', 'bnfg bs rbr', NULL, 'проба', x'343335343366676466736266206262736662737272736E72677420207220', '9', NULL),
@@ -354,6 +321,12 @@ INSERT INTO `position` VALUES
 (20, '45', '5353', 'reger', 4.00, '2019-02-20 14:14:09', 'greger', NULL, 'reger', x'7265676572', '10', NULL),
 (21, 'rte', '34534534', 'ert434', 5.00, '2019-02-20 14:20:30', 'regerge', NULL, '43534', x'746572746572', '10', NULL),
 (22, '54343', '45345345345', 'ergerg', 5.00, '2019-02-20 18:56:14', 'retgretgre', NULL, '5343', x'726567726567', '10', NULL);
+
+-- 
+-- Вывод данных для таблицы `order`
+--
+INSERT INTO `order` VALUES
+(1, 2, 11, 2, '2021-02-20 19:00:00', '2024-02-20 19:00:00', 'open', NULL, '100');
 
 -- 
 -- Вывод данных для таблицы migration
@@ -366,11 +339,6 @@ INSERT INTO migration VALUES
 -- Вывод данных для таблицы messages
 --
 -- Таблица fp_base.messages не содержит данных
-
--- 
--- Вывод данных для таблицы groups
---
--- Таблица fp_base.groups не содержит данных
 
 -- 
 -- Вывод данных для таблицы country
