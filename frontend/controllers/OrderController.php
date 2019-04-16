db5a238d33f456c62aad498ea65d9359028a1489
dmkorolev@yandex.ru <dmkorolev@yandex.ru>
2019-04-16 23:07:42 +0300

    orders

--- frontend/controllers/OrderController.php
+++ frontend/controllers/OrderController.php
@@ -2,9 +2,13 @@
 
 namespace frontend\controllers;
 
+use Throwable;
 use Yii;
 use frontend\Helpers\OrganizationHelper;
 use frontend\models\Organization;
+use yii\base\InvalidConfigException;
+use yii\data\ActiveDataProvider;
+use yii\db\StaleObjectException;
 use yii\helpers\ArrayHelper;
 use frontend\models\Order;
 use frontend\models\OrderSearchModel;
@@ -36,33 +40,36 @@ class OrderController extends Controller
 
     /**
      * Lists all Order models.
+     * @param int $id
      * @return mixed
      */
     public function actionIndex($id = 0)
-    { 
-        $positions = Position::findAll([
-        'org_id' => OrganizationHelper::getOrg()
-        ]);
-        foreach ($positions as $position)
-        {
-            $order = Order::find()->where(['position_id' => $position])->one();
-            if(isset($order)) $orders[] = $order;
-        }
-
-        foreach ($orders as $order)
-        {
-            $items[] = $order->position;                
-        }        
+    {
+        $or = Order::find()
+                        ->select([Organization::tableName() . '.name', Order::tableName() . '.org_id'])
+                        ->joinWith('position')
+                        ->joinWith('org')
+                        ->where([Position::tableName() . '.org_id' => OrganizationHelper::getCurrentOrg()->id])
+                        ->distinct()
+                        ->all();
 
         if ($id == 0)
         {
-            $id = key($items);
+            $id = current(array_column($or, 'org_id'));
         }
-        
+
+//        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+        $searchModel = new OrderSearchModel();
+        $dataProvider = new ActiveDataProvider([
+            'query' => Order::find()
+                                ->joinWith('position')
+                                ->where([Order::tableName() . '.org_id' => $id]),
+        ]);
+
         return $this->render('index', [
-            'items' => $items,
-            'id' => $id,
-            'org' => Organization::getOrgListByUserId(Yii::$app->user->id)
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+            'items' => $or,
         ]);
     }
 
@@ -123,6 +130,8 @@ class OrderController extends Controller
      * @param integer $id
      * @return mixed
      * @throws NotFoundHttpException if the model cannot be found
+     * @throws Throwable
+     * @throws StaleObjectException
      */
     public function actionDelete($id)
     {
--- frontend/views/order/index.php
+++ frontend/views/order/index.php
@@ -1,188 +1,80 @@
 <?php
 
+use yii\data\ActiveDataProvider as ActiveDataProviderAlias;
 use yii\helpers\Html;
-use yii\grid\GridView;
-
 use frontend\models\Order;
-use yii\widgets\Pjax;
+use \yii\grid\GridView;
+use frontend\components\NumberColumn;
 
-$this->registerJs('
-    $(".delete-prod").on("click", function() {
-        return confirm("Вы действительно хотите удалить товар?"); 
-    });
-');
+/* @var Order[] $items  */
+/** @var ActiveDataProviderAlias $dataProvider */
+
 ?>
 
 <style>
-	* {
-		box-sizing: border-box;
-	}
-    a, a:link, a:visited, a:hover, a:active {
-        text-decoration: none;
-        cursor: pointer;
-        color: #fff;
-    }
-    a:hover {
-        color: #000;
-    }
-    a:hover span {
-        color: #fff;
-    }
-    .content-products {
-        width: 100%;
-        height: 100vh;
-        background-color: #fff;
-    }
-    .products-list {
-        width: 45%;
-        float: left;
-        height: 100%;
-        padding-left: 3px;
-        /*border: 1px solid #fff;*/
-        /*border-radius: 10px 10px;*/
-    }
-    .products-list-inner {
-        width: 100%;
-        height: 100%;
-        overflow: auto;
-        padding-top: 20px;
-        padding-left: 20px;
-        padding-right: 20px;
-    }
-    .product-details {
-        width: 55%;
-        float: left;
-        height: 100%;
-        padding-left: 3px;
-        margin: 0;
-    }
-    .product-details-inner {
-        color: #fff;
-        overflow: auto;
-        width: 100%;
-        height: 100%;
-        padding-top: 20px;
-        padding-left: 50px;
-        padding-right: 50px;
-    }
-    .product-item-inner:hover {
-        color: #000;
-        background: #eee;
-        width: 100%;
-        border-radius: 10px 10px;
-        padding: 10px 5px;
-    }
-    .product-item-active {
-        color: #000;
-        background: #eee;
-        width: 100%;
-        border-radius: 10px 10px;
-        padding: 10px 5px;
-    }
-    .product-item-inner {
-        padding: 10px 5px;
-        width: 100%;
-    }
-    .product-item {
-        width: 100%;
-        padding: 5px 0;
-        color: #fff;
-    }
-    .product-toolbox {
-        padding: 20px 0 40px 20px;
-    }
-    .product-toolbox-inner {
+    .int {
         padding: 10px;
+        display: table;
+    }
+    .content-orders {
+        height: 100%;
+        width: 100%;
+    }
+    .list-org {
+        width: 40%;
         float: left;
     }
-    .product-toolbox-inner span.glyphicon-remove {
-        color: #d33;
-    }
-    .product-toolbox-inner span.glyphicon-remove:hover {
-        color: #f00;
-    }
-    .product-toolbox-inner span.glyphicon-plus {
-        color: #3d3;
-    }
-    .product-toolbox-inner span.glyphicon-plus:hover {
-        color: #0f0;
+    .list-orders {
+        width: 40%;
+        float: left;
     }
 </style>
-<div class="content-products clearfix">
-    <div class="products-list">
-        <?php if (Yii::$app->controller->action->id == 'index'): ?>
-            <div class="product-toolbox bgcolor">
-                <div class="product-toolbox-inner">
-                    <?=
-                        Html::a(Html::tag('span', '', ['class' => "glyphicon glyphicon-plus"]), '/order/create', [
-                            'title' => Yii::t('app', 'Add'),
-                            'data-pjax' => '1',
-                        ])
-                    ?>
+
+<div class="content-orders clearfix">
+    <div class="list-org">
+        <div class="list-org-inner">
+            <?php foreach ($items as $order) : ?>
+                <div class="product-item">
+                    <?= Html::a(Html::tag('p', $order->org->name . '<br/>' . "order's amount", ['class' => 'int']), '/order/index?id=' . $order->org_id) ?>
                 </div>
-                <div class="product-toolbox-inner">
-                    <?php if (!empty($items)) : ?>
-                        <?=
-                            Html::a(Html::tag('span', '', ['class' => "glyphicon glyphicon-remove"]), '/order/delete?id=' . $items[$id]->id, [
-                                'title' => Yii::t('app', 'Delete'),
-                                'data-pjax' => '1',
-                                'class' => 'delete-prod',
-                                'data' => [
-                                    'method' => 'post',
-                                    'params' => ['id' => $items[$id]->id],
-                                ],
-                            ])
-                        ?>
-                    <?php endif; ?>
-                </div>
-            </div>
-        <?php endif; ?>
-    	<div class="products-list-inner bgcolor">
-        	<?php
-                if (Yii::$app->controller->action->id == 'create')
-                {
-                    echo $this->render('/order/create', [
-                        'id' => $id,
-                        'items' => $items,
-                        'org' => $org
-                    ]);
-                }
-        	?>
-            <?php if (Yii::$app->controller->action->id == 'index'): ?>
-                <?php foreach ($items as $product): ?>
-                    <div class="product-item">
-                        <?= Html::a(Html::tag('div', $product->name, ['class' => $product->id == $id ? 'order-item-active' : 'order-item-inner']), ['/order/index?id=' . $product->id]) ?>
-                    </div>
-                <?php endforeach; ?>
-            <?php endif; ?>
-    	</div>
+            <?php endforeach; ?>
+        </div>
     </div>
-    <div class="product-details">
-        <div class="product-details-inner bgcolor">
-            <?php
-                Pjax::begin();
-
-                if (Yii::$app->controller->action->id == 'update')
-                {
-                    echo $this->render('/order/update', [
-                        'id' => $id,
-                        'item' => $items[$id],
-                        'org' => $org
-                    ]);
-                }
-
-                if (Yii::$app->controller->action->id == 'index')
-                {
-                    if (!empty($items))
-                    {
-                        echo $this->render('details', [
-                            'id' => $id,
-                            'item' => $items[$id]
-                        ]);
-                    }
-                }
-
-                Pjax::end();
+    <div class="list-orders">
+        <div class="list-orders-inner">
+            <?=
+                GridView::widget([
+                    'dataProvider' => $dataProvider,
+                    'showFooter' => true,
+                    'columns' => [
+                        ['class' => 'yii\grid\SerialColumn'],
+                        'id',
+                        'position.name',
+                        [
+                            'attribute' => 'position.price',
+                            'class' => NumberColumn::className(),
+                        ],
+                        'number',
+                        [
+                            'attribute' => 'amount',
+                            'content' => function($data) {
+                                return $data->position->price * $data->number;
+                            },
+                            'filter' => false,
+                            'class' => NumberColumn::className(),
+                        ],
+//                        [
+//                            'attribute' => 'amount',
+//                            'content' => function($data) {
+//                                return $data->position->price * $data->number;
+//                            },
+//                            'filter' => false,
+//                            'footer' => Order::getTotal($dataProvider->models)
+//                        ],
+                        'date_from',
+                        'date_to',
+                    ],
+                ]);
             ?>
         </div>
     </div>
